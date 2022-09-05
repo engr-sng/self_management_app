a9929eb59635545f53e4ab4458e309f3d8c069e3
EC2 Default User <108569722+ShingoIshikawa@users.noreply.github.com>
2022-09-05 07:38:48 +0000

    [add]ユーザー・アドミンの認証機能実装、JqueryとFontAwsomeの環境設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "324a5f6975524fda9e31cd5611a30717.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
