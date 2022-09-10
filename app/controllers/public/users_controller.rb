786555bc7d2ce38efff34cc5bd32a6e9437975a5
EC2 Default User <108569722+ShingoIshikawa@users.noreply.github.com>
2022-09-10 09:23:35 +0000

    [add]プロフィール画像投稿機能、サーバー側でのリサイズ処理の実装

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -28,6 +28,6 @@ class Public::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:display_name, :user_name, :email)
+    params.require(:user).permit(:display_name, :user_name, :email, :profile_image)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,18 @@ class User < ApplicationRecord
   has_many :project_members
   has_many :child_tasks
 
+  has_one_attached :profile_image
+
   validates :display_name, presence: true
   validates :user_name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
+
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
 
 end
