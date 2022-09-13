f5b598732c3be1d5faec9eafd928cb848cdf1a0f
EC2 Default User <108569722+ShingoIshikawa@users.noreply.github.com>
2022-09-13 08:56:11 +0000

    [add]管理者レイアウト調整

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,18 @@
 class Admin::HomesController < ApplicationController
+
+  before_action :authenticate_admin!
+
   def top
+    @users = User.all
+    invalid_user = @users.where(is_deleted: true).count
+    valid_user = @users.where(is_deleted: false).count
+
+    if @users.where(is_deleted: true).count == 0
+      @withdrawal_rate = "バ"
+    elsif @users.where(is_deleted: false).count == 0
+      @withdrawal_rate = "カ"
+    else
+      @withdrawal_rate = ((invalid_user*100)/valid_user)
+    end
   end
 end
